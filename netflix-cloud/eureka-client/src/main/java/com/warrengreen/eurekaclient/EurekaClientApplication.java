84e74204eef11417c16d66b1c3039f6d7de14201
Warren Green <warren.green84@gmail.com>
2017-01-17 21:34:36 +0000

    adding netflix cloud demos

---
+++ netflix-cloud/eureka-client/src/main/java/com/warrengreen/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,27 @@
+package com.warrengreen.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class EurekaClientApplication {
+	  
+	@RequestMapping(value = "/available")
+	  public String available() {
+	    return "Spring in Action";
+	  }
+
+	  @RequestMapping(value = "/checked-out")
+	  public String checkedOut() {
+	    return "Spring Boot in Action";
+	  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
